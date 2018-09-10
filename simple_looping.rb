5899abe5640274f7330cf27810bef15a478bf2e0
Randy Herasme <randyherasme6@gmail.com>
2018-09-10 20:34:44 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -4,7 +4,7 @@ def loop_iterator(number_of_times)
   i=0
   loop do
     i+=1
-    puts phrase
+    puts "Welcome to Flatiron School's Web Development Course!"
     if i>number_of_times
       break
     end
