adebd82371f02772bde473e71893c6af6fa859e2
Randy Herasme <randyherasme6@gmail.com>
2018-09-10 20:42:34 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -13,7 +13,7 @@ def loop_iterator(number_of_times)
 end
 
 def times_iterator(number_of_times)
-  number_of_times.times.do
+  number_of_times.times do
     puts "Welcome to Flatiron School's Web Development Course!"
   end
   phrase = "Welcome to Flatiron School's Web Development Course!"
